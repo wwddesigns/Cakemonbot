97e3b48a9f987fb8bee9b4942c9ef1fce155605f
Shaun Whitson <64608072+wwddesigns@users.noreply.github.com>
2021-09-17 14:09:44 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  subdomain: "api",
+  consumer_key: "KD0yHQFWMeIQ3vjMmRefWdRm3",
+  consumer_secret: "ix3QESATJIanzw2vxaTkmbgrckgdsxjnPMGeDGVBe5K6PxRZRX",
+  access_token_key: "2321203640-3TqFo5k03lQ36H3FrOctCtCyJk5T35wE46o5BBn",
+  access_token_secret: "SCSByd6uk2GfPVfvX5pz7oa38BWtUMBORi19LOHDbVkGh"
+};
---
+++ motivationalReplies.js
@@ -0,0 +1,3 @@
+module.exports = [
+    //CAKE $MONSTA Great fundamentals with #NFT integrations, not a reflection clone, but a unique reward system. Long-term project. Our $CAKE vault is even bigger than our total liquidity. cake.monster to get started.#TheBeastOfBSC #cakelombos #1BillionMcapIsProgrammed
+]
